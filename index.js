bd4b573347827295210ab24f09acec9141c9d404
ayesha <fxolbird@gmail.com>
2021-10-17 03:34:22 -0700

    changed

---
+++ index.js
@@ -0,0 +1,16 @@
+ let path = require('path');
+// console.log('we are working with path module ');
+
+// console.log('It works with file and dir paths');
+
+ //console.log("here's the dir name", + " "+path.dirname('D:/NODE.Js/path Module/index.js') );
+
+// console.log("here's the extension name", + " "+path.extname('D:/NODE.Js/path Module/index.js') );
+
+// console.log("here's the base name", + " "+path.basename('D:/NODE.Js/path Module/index.js') );
+
+// let myPath = path.parse('D:/NODE.Js/path Module/index.js') ;
+
+// // console.log(myPath.name);
+
+console.log(path.parse('D:/NODE.Js/path Module/index.js'));
